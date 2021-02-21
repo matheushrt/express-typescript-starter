32e73e5f783bbb4f7475723a9492c9d2b872a0d3
Matheus Souza <matheushrt@gmail.com>
2021-02-21 20:54:23 -0300

    chore: initialize helmet and remove morgan

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,13 @@ import express from 'express';
 import path from 'path';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-import logger from 'morgan';
+import helmet from 'helmet';
 
 import { home } from './routes';
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
