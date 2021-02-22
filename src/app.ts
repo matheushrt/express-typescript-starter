994a1711a9ae64394191447be1137a75d93788a9
Matheus Souza <matheushrt@gmail.com>
2021-02-22 01:11:34 -0300

    chore: apply pino logger middleware

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,15 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import helmet from 'helmet';
 
+import { expressLogger } from './config/logger';
+
 import { home } from './routes';
 
 const app = express();
 
 app.use(helmet());
 app.use(cors());
+app.use(expressLogger);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
